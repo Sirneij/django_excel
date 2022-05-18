8d38ef5eabfaa7284285c47f881202464559899f
John Idogun <sirneij@gmail.com>
2022-05-18 03:17:45 +0100

    Tests wrapped up

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -8,9 +8,6 @@ register = template.Library()
 
 @register.filter(name='currency')
 def currency(value):
-    try:
-        locale.setlocale(locale.LC_ALL, 'en_NG.UTF-8')
-    except:
-        locale.setlocale(locale.LC_ALL, '')
+    locale.setlocale(locale.LC_ALL, 'en_NG.UTF-8')
     loc = locale.localeconv()
     return locale.currency(decimal.Decimal(value), loc['currency_symbol'], grouping=True)
--- tests/core/test_tasks.py
+++ tests/core/test_tasks.py
@@ -1,13 +1,36 @@
+from unittest.mock import patch
+
 from django.core import mail
 from django.test import TestCase
 
 from core.models import Coins
-from core.tasks import export_data_to_excel
+from core.tasks import export_data_to_excel, get_coins_data_from_coingecko_and_store
 
 
 class CoinTasksTests(TestCase):
+    def test_get_coins_data_from_coingecko_and_store(self):
+        '''Test get_coins_data_from_coingecko_and_store.'''
+
+        with patch('core.tasks.requests.get') as mock_get:
+            mock_get.return_value.coin_data = [
+                {
+                    'symbol': 'btc',
+                    'name': 'Bitcoin',
+                    'image': 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+                    'current_price': 12644080,
+                    'market_cap': 240714282203755,
+                    'market_cap_rank': 1,
+                    'price_change_24h': 197155,
+                    'total_supply': 21000000.0,
+                }
+            ]
+
+            get_coins_data_from_coingecko_and_store()
+
+        mock_get.assert_called_once()
+
     def test_export_data_to_excel(self):
-        """Test export_data_to_excel task."""
+        '''Test export_data_to_excel task.'''
         Coins.objects.create(
             name='bitcoin', symbol='btc', current_price=12000000, price_change_within_24_hours=500, market_cap=210000000
         )
