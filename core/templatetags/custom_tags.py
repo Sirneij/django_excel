3575175fea6937af2a017b2a0736d606fcda3f57
John Idogun <sirneij@gmail.com>
2022-05-18 05:19:56 +0100

    README.md

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -7,8 +7,11 @@ register = template.Library()
 
 
 @register.filter(name='currency')
-def currency(value: str) -> str:
-    """Format currency in naira."""
-    locale.setlocale(locale.LC_ALL, 'en_NG.UTF-8')
+def currency(value):  # type: ignore
+    """Format currency."""
+    try:
+        locale.setlocale(locale.LC_ALL, 'en_NG.UTF-8')
+    except (locale.Error, ValueError):
+        locale.setlocale(locale.LC_ALL, '')
     loc = locale.localeconv()
     return locale.currency(decimal.Decimal(value), loc['currency_symbol'], grouping=True)  # type: ignore
