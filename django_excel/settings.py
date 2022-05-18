b945ac8fc436ba08aeef5fb2eaa5005e54df58fb
John Idogun <sirneij@gmail.com>
2022-05-18 04:58:45 +0100

    Python version in django.yml

--- django_excel/settings.py
+++ django_excel/settings.py
@@ -131,6 +131,7 @@ USE_TZ: bool = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL: str = 'static/'
+STATIC_ROOT: str = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
